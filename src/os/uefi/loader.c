08e79898f5b7e937f28af5be0496c9713ae61b19
Akihabara-JB03 <akabanestation1007@gmail.com>
2026-08-30 21:15:16 +0900

    Update loader.c

--- src/os/uefi/loader.c
+++ src/os/uefi/loader.c
@@ -1,1 +1,6 @@
 #include "include/loader.h"
+
+EFI_STATUS load_kernel(EFI_HANDLE IH,EFI_SYSTEM_TABLE *ST) {
+  EFI_LOADED_IMAGE_PROTOCOL *LoadedImage;
+  ST->BootServices->OpenProtocol(IH,&gEfiLoadedImageProtocolGuid,&LoadedImage,IH,NULL,EFI_OPEN_PROTOCOL_GET_PROTOCOL);
+}
