785e0c204efb6dc7d6b079efd5b9013d1f814d32
Akihabara-JB03 <akabanestation1007@gmail.com>
2026-08-30 21:44:48 +0900

    Update loader.c

--- src/os/uefi/loader.c
+++ src/os/uefi/loader.c
@@ -2,5 +2,39 @@
 
 EFI_STATUS load_kernel(EFI_HANDLE IH,EFI_SYSTEM_TABLE *ST) {
   EFI_LOADED_IMAGE_PROTOCOL *LoadedImage;
-  ST->BootServices->OpenProtocol(IH,&gEfiLoadedImageProtocolGuid,&LoadedImage,IH,NULL,EFI_OPEN_PROTOCOL_GET_PROTOCOL);
+    EFI_STATUS status = ST->BootServices->OpenProtocol(IH,
+    &gEfiLoadedImageProtocolGuid,
+    &LoadedImage,
+    IH,
+    NULL,
+    EFI_OPEN_PROTOCOL_GET_PROTOCOL
+  );
+  if (EFI_ERROR(status)) {
+    return status;
+  }
+  EFI_SIMPLE_FILE_SYSTEM_PROTOCOL *SFSP;
+  status = ST->BootServices->OpenProtocol(LoadedImage->DeviceHandle,
+    &gEfiSimpleFileSystemProtocolGuid,
+    &SFSP,
+    IH,
+    NULL,
+    EFI_OPEN_PROTOCOL_GET_PROTOCOL
+  );
+  if (EFI_ERROR(status)) {
+    return status;
+  }
+  EFI_FILE_PROTOCOL *Root;
+  status = SFSP->OpenVolume(SFSP,&Root);
+  if (EFI_ERROR(status)) {
+    return status;
+  }
+  EFI_FILE_PROTOCOL *KernelFile;
+  status = Root->Open(&KernelFile,
+    L"kernel.elf",
+    EFI_FILE_MODE_READ,
+    0
+  );
+  if (EFI_ERROR(status)) {
+    return status;
+  }
 }
